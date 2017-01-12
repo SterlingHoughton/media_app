1562040dbbc8a3c57f57711b8929e06cd304cf32
Sterling Houghton <sterling.houghton@nike.com>
2017-01-12 09:22:29 -0800

    basic server config setup, installed express, socket.io, and pug

--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,39 @@
 import "source-map-support/register";
 
+import express from "express";
+import http from "http";
+import socketIo from "socket.io"
 
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+//SETUP
+const app = express();
+const server = new http.Server(app);
+const io = socketIo(server);
+
+//CLIENT WEBPACK
+
+//EXPRESS CONFIG
+app.set("view engine", "pug");
+app.use(express.static("public"));
+
+const useExternalStyles = !isDevelopment;
+app.get("/", (reqeust, response) => {
+	response.render("index", {
+		useExternalStyles
+	});
+});
+
+//MODULES
+
+//SOCKET
+
+//STARTUP
+const port = process.env.PORT || 3000;
+function startServer() {
+	server.listen(port, () => {
+		console.log(`Started http server on ${port}`);
+	});
+}
+
+startServer();
